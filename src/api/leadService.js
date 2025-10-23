9bf1f13a834a42727ad14d3c4d25d702b2a680ad
chandrashekhar3337 <shekharara926290@gmail.com>
2025-10-23 18:52:43 +0530

    change

--- src/api/leadService.js
+++ src/api/leadService.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api/v",
+  baseURL: import.meta.env.VITE_API_URL || "https://urban-lms-backend.onrender.com",
   timeout: 10000,
 });
 
 // fetch all leads
-export const fetchLeads = () => API.get("/");
+export const fetchLeads = () => API.get("/api/v1/users/");
 
 // create a website lead
-export const postWebsiteLead = (payload) => API.post("/website", payload);
+export const postWebsiteLead = (payload) => API.post("/api/v1/users/website", payload);
 
 // simulate meta lead
-export const postMetaLead = (payload) => API.post("/meta", payload);
+export const postMetaLead = (payload) => API.post("/api/v1/users/meta", payload);
 
 // simulate google lead
-export const postGoogleLead = (payload) => API.post("/google", payload);
+export const postGoogleLead = (payload) => API.post("/api/v1/users/google", payload);
 
 export default API;
